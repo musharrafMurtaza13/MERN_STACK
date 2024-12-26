51f9cd2756fa306a53fcac316f0a7d4ba20b586e
MusharrafAhmed <M.ahmed@epicmetry.com>
2024-12-26 18:42:14 +0500

    database with mongo db connection work

--- ch-02/controllers/taskController.js
+++ ch-02/controllers/taskController.js
@@ -1,21 +1,43 @@
-const getTasks = (req, res) => {
-	res.status(200).json({ message: 'Get All Tasks' });
-}
+const asyncHandler = require('express-async-handler')
+const Task = require('../models/taskModel')
 
-const setTask = (req, res) => {
+const getTasks = asyncHandler(async (req, res) => {
+	const tasks = await Task.find()
+	res.status(200).json(tasks);
+})
+
+const setTask = asyncHandler(async (req, res) => {
 	if(!req.body.text){
         res.status(400)
         throw new Error('Please enter a task')
     }
-	res.status(200).json({ message: 'Create Task' });
-}
+	const task = await Task.create({ text: req.body.text })
+	res.status(200).json(task);
+})
 
-const updateTask = (req, res) => {
-	res.status(200).json({ message: `Task ${req.params.id} updated.` });
-}
+const updateTask = asyncHandler(async (req, res) => {
+	const task = await Task.findById(req.params.id)
 
-const deleteTask = (req, res) => {
-	res.status(200).json({ message: `Task ${req.params.id} deleted.` });
-}
+	if (!task) {
+		res.status(400)
+		throw new Error('Task not found')
+	}
+
+	const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, {  new: true })
+	res.status(200).json(updatedTask)
+})
+
+const deleteTask = asyncHandler(async (req, res) => {
+	const task = await Task.findById(req.params.id)
+
+	if (!task) {
+		res.status(400)
+		throw new Error('Task not found')
+	}
+
+	await Task.findByIdAndDelete(req.params.id)
+
+	res.status(200).json({ id: req.params.id })
+})
 
 module.exports = { getTasks, setTask, updateTask, deleteTask }
